f7a9d15c28e451ced021cf3aff8310917e89422c
iuejeong <dmlgus3585@naver.com>
2023-03-13 13:35:52 +0900

    css 마무리, javascript 찍먹

---
+++ web_basic/javascript/js/js1.js
@@ -0,0 +1,15 @@
+// alert("알림창을 확인해주세요.");
+// alert("두 번째 알림창");
+// alert(10 + 20);
+// console.log("println");
+// let flag = confirm("선택해주세요");
+// console.log(flag);
+// let name2 = prompt("이름을 입력해주세요");
+// console.log(name2);
+let test;
+console.log(test)
+
+let num1 = parseInt(prompt("첫 번째 수"))                    
+let num2 = parseInt(prompt("두 번째 수"))
+
+console.log(num1 + num2)
